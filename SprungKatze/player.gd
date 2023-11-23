02873b2e620db8c207d5cde482a60c72685a3989
Stefan <hoermann@cpg-gilching.de>
2023-11-23 15:06:18 +0100

    Stand 23.11.2023
    Die Gegenstand-Szene kam neu dazu. Im Skript von Gegenstand ist die Bewegung zu sehen.

--- SprungKatze/player.gd
+++ SprungKatze/player.gd
@@ -9,20 +9,17 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	$AnimatedSprite2D.play("walk")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		$AnimatedSprite2D.play("jump")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AudioStreamPlayer.play()
+		
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
