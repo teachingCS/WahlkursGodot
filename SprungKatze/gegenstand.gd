e12b85d9e6d4b8aba5d6140c23a2fd78a28ccf1a
Stefan <hoermann@cpg-gilching.de>
2024-01-11 14:59:48 +0100

    Stand 11.01.2024
    Kollisionen und Ebenen festgelegt
    Menü hinzugefügt

--- SprungKatze/gegenstand.gd
+++ SprungKatze/gegenstand.gd
@@ -15,3 +15,7 @@ func _process(delta):
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	# damit das Objekt gelöscht wird, wenn es nicht mehr sichtbar ist
 	queue_free()
+
+
+func _on_body_entered(body):
+	get_tree().change_scene_to_file("res://menu.tscn")
